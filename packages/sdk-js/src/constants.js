e8bc70994598e48073755f0d6fd4a531976e09dc
ZKVerse <syekebaj57@gmail.com>
2025-11-25 06:52:36 +0000

    Create constants.js

---
+++ packages/sdk-js/src/constants.js
@@ -0,0 +1,11 @@
+// constants.js
+
+/**
+ * Known deposit statuses.
+ * These should match the backend implementation.
+ */
+export const DepositStatus = Object.freeze({
+  PENDING: "pending",
+  FORWARDED: "forwarded",
+  FAILED: "failed"
+});
